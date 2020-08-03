e8cbf4219c87855b01366b3d7dc05980a4ad1b87
z <z@example.com>
2020-08-03 19:42:09 +0800

    c3-6 global.scss和reset.scss设置

--- book-chapter/vue.config.js
+++ book-chapter/vue.config.js
@@ -1,12 +1,35 @@
+/*
+function mock(app, url, data) {
+  app.get(url, (request, response) => {
+    response.json(data)
+  })
+}
+
+const homeData = require('./src/mock/bookHome')
+const shelfData = require('./src/mock/bookShelf')
+const listData = require('./src/mock/bookList')
+const flatListData = require('./src/mock/bookFlatList')
+*/
+
 module.exports = {
+  baseUrl: process.env.NODE_ENV === 'production'
+    ? './'
+    : '/',
   devServer: {
-    port: 3000,
+    /*
+    before(app) {
+      mock(app, '/book/home', homeData)
+      mock(app, '/book/shelf', shelfData)
+      mock(app, '/book/list', listData)
+      mock(app, '/book/flat-list', flatListData)
+    }
+    */
   },
   configureWebpack: {
-    entry: {
-      app: './src/main.js',
-      style: [
-      ]
+    performance: {
+      hints: 'warning',
+      maxAssetSize: 524288 * 10,
+      maxEntrypointSize: 524288 * 10
     }
   }
 }
