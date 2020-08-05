b6a85631f61418676d15d1927dc4cfae8fba110b
z <Lysander@example.com>
2020-08-05 22:49:00 +0800

    adjusting ebook.vue style

--- book-free/src/Ebook.vue
+++ book-free/src/Ebook.vue
@@ -1,8 +1,42 @@
 <template>
   <div class="ebook">
+    <div class="title-wrapper">
+      <div class="left">
+        <span class="icon-back icon"></span>
+      </div>
+      <div class="right">
+        <div class="icon-wrapper">
+          <span class="icon-cart icon "></span>
+        </div>
+        <div class="icon-wrapper">
+          <span class="icon-person icon "></span>
+        </div>
+        <div class="icon-wrapper">
+          <span class="icon-more icon "></span>
+        </div>
+      </div>
+    </div>
     <div class="read-wrapper">
       <div id="read">
-
+        <div class="mask">
+          <div class="left" @click="prevPage"></div>
+          <!--          <div class="center" @click="toggleTitleAndMenu"></div>-->
+          <div class="right" @click="nextPage"></div>
+        </div>
+      </div>
+    </div>
+    <div class="menu-wrapper">
+      <div class="icon-wrapper">
+        <div class="icon-menu icon"></div>
+      </div>
+      <div class="icon-wrapper">
+        <div class="icon-progress icon"></div>
+      </div>
+      <div class="icon-wrapper">
+        <div class="icon-bright icon"></div>
+      </div>
+      <div class="icon-wrapper">
+        <div class="icon-a icon"></div>
       </div>
     </div>
   </div>
@@ -33,11 +67,17 @@ export default {
       this.rendition.display()
 
     },
-    nextPage() {
-      this.rendition && this.rendition.next()
-    },
+    // 上一页
     prevPage() {
-      this.rendition && this.rendition.prev()
+      if (this.rendition) {
+        this.rendition.prev()
+      }
+    },
+    // 下一页
+    nextPage() {
+      if (this.rendition) {
+        this.rendition.next()
+      }
     },
   },
 
@@ -55,6 +95,62 @@ export default {
 .ebook {
   position: relative;
 
+  .title-wrapper {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: px2rem(48);
+    z-index: 101;
+    display: flex;
+    width: 100%;
+    box-shadow: 0 px2rem(8) px2rem(8) rgba(0, 0, 0, .15); /* 0 , 垂直方向, 水平方向 */
+    background: whitesmoke;
+
+    .left {
+      flex: 0 0 px2rem(60);
+      @include center;
+
+    }
+
+    .menu-wrapper {
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      z-index: 100;
+      width: 100%;
+      height: 100%;
+      box-shadow: 0 px2rem(-8) px2rem(8) rgba(0, 0, 0, .15); /* 0 , 垂直方向, 水平方向 */
+      .icon-wrapper {
+        flex: 1;
+        @include center;
+
+        .icon-progress {
+          font-size: px2rem(24);
+        }
+      }
+    }
+
+    .right {
+      flex: 1;
+      display: flex;
+      justify-content: flex-end;
+
+      .icon-wrapper {
+        flex: 0 0 px2rem(40);
+        @include center;
+
+        .icon-cart {
+          font-size: px2rem(22);
+        }
+      }
+    }
+
+    .center {
+
+    }
+  }
+
   .read-wrapper {
     .mask {
       position: absolute;
@@ -65,18 +161,19 @@ export default {
       height: 100%;
 
       .left {
-
+        flex: 0 0 px2rem(100);
       }
 
       .center {
-
+        flex: 1;
       }
 
       .right {
-
+        flex: 0 0 px2rem(100);
       }
     }
   }
+
 
 }
 </style>
