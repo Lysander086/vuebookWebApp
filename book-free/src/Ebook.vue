5172f7005bf6443da31141730eaba9a4fd21683d
z <Lysander@example.com>
2020-08-06 15:11:00 +0800

    翻页功能实现

--- book-free/src/Ebook.vue
+++ book-free/src/Ebook.vue
@@ -1,15 +1,26 @@
 <template>
- <div class="ebook">
-   <div class="read-wrapper">
-     <div id="read">
+  <div class="ebook">
+    <div class="read-wrapper">
+      <div id="read">
+        <div class="mask">
+          <div class="left" @click="prevPage">
 
-     </div>
-   </div>
- </div>
+          </div>
+          <div class="center">
+
+          </div>
+          <div class="right" @click="nextPage">
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 import Epub from 'epubjs'
+
 global.epub = Epub
 const DOWNLOAD_URL = '/static/software.epub'
 
@@ -23,12 +34,55 @@ export default {
       })
       this.rendition.display()
 
+    },
+    prevPage(){
+      this.rendition && this.rendition.prev()
+    },
+    nextPage(){
+      this.rendition && this.rendition.next()
     }
   },
-  mounted(){
+  mounted() {
     this.showEpub()
   }
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@import 'assets/styles/global';
+
+.ebook {
+  position: relative;
+
+  .read-wrapper {
+    .mask {
+      position: absolute;
+      top: 0;
+      left: 0;
+      z-index: 100;
+      display: flex;
+      width: 100%;
+      height: 100%;
+      /*background: yellow;*/
+      .left {
+        flex: 0 0 px2rem(100);
+        /*background: yellow;*/
+      }
+
+      .center {
+        flex: 1;
+        /*background: rebeccapurple;*/
+
+      }
+
+      .right {
+        flex: 0 0 px2rem(100);
+        /*background: deeppink;*/
+
+
+      }
+    }
+  }
+}
+
+</style>
