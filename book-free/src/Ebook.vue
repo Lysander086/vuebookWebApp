781682aa1a093fd89f9b4a833dde182d2d5047fc
z <Lysander@example.com>
2020-08-06 16:50:07 +0800

    3-5 菜单栏 过度样式完成

--- book-free/src/Ebook.vue
+++ book-free/src/Ebook.vue
@@ -1,18 +1,28 @@
 <template>
   <div class="ebook">
-    <div class="title-wrapper">
-      <div class="left"><span class="icon-back icon"></span></div>
-      <div class="right icon-wrapper">
-        <div><span class="icon-cart icon"></span></div>
-        <div><span class="icon-person icon"></span></div>
-        <div><span class="icon-more icon"></span></div>
+    <transition name="slide-down">
+      <div class="title-wrapper" v-show="ifTitleAndMenuShow">
+        <div class="left">
+          <span class="icon-back icon"></span>
+        </div>
+        <div class="right ">
+          <div class="icon-wrapper">
+            <div><span class="icon-cart icon"></span></div>
+          </div>
+          <div class="icon-wrapper">
+            <div><span class="icon-person icon"></span></div>
+          </div>
+          <div class="icon-wrapper">
+            <div><span class="icon-more icon"></span></div>
+          </div>
+        </div>
       </div>
-    </div>
-    <div class="read-wrapper">1
+    </transition>
+
+    <div class="read-wrapper">
       <div id="read">
         <div class="mask">
           <div class="left" @click="prevPage">
-
           </div>
           <div class="center" @click="toggleTitleAndMenu">
 
@@ -23,6 +33,22 @@
         </div>
       </div>
     </div>
+    <transition name="slide-up">
+      <div class="menu-wrapper" v-show="ifTitleAndMenuShow">
+        <div class="icon-wrapper ">
+          <span class="icon-menu icon"></span>
+        </div>
+        <div class="icon-wrapper ">
+          <span class="icon-progress icon"></span>
+        </div>
+        <div class="icon-wrapper ">
+          <span class="icon-bright icon"></span>
+        </div>
+        <div class="icon-wrapper ">
+          <span class="icon-a icon">A</span>
+        </div>
+      </div>
+    </transition>
   </div>
 </template>
 
@@ -33,6 +59,13 @@ global.epub = Epub
 const DOWNLOAD_URL = '/static/software.epub'
 
 export default {
+  data() {
+    return {
+      ifTitleAndMenuShow: false
+
+    };
+  },
+
   methods: {
     showEpub() {
       this.book = new Epub(DOWNLOAD_URL)
@@ -48,6 +81,9 @@ export default {
     },
     nextPage() {
       this.rendition && this.rendition.next()
+    },
+    toggleTitleAndMenu() {
+      this.ifTitleAndMenuShow = !this.ifTitleAndMenuShow
     }
   },
   mounted() {
@@ -78,9 +114,6 @@ export default {
       @include center;
     }
 
-    .center {
-    }
-
     .right {
       flex: 1;
       display: flex;
@@ -89,7 +122,20 @@ export default {
       .icon-wrapper {
         flex: 0 0 px2rem(40);
         @include center;
+        margin-bottom: auto;
       }
+    }
+
+    &.slide-down-enter, &slide-down-leave-to {
+      transform: translate3d(0, -100%, 0);
+    }
+
+    &.slide-down-enter-to, &slide-down-leave {
+      transform: translate3d(0, 0, 0);
+    }
+
+    &.slide-down-enter-active, &slide-down-leave-active {
+      transition: all .1s linear;
     }
   }
 
@@ -120,6 +166,44 @@ export default {
 
 
       }
+    }
+  }
+
+  .menu-wrapper {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    z-index: 101;
+    display: flex;
+    width: 100%;
+    height: px2rem(48);
+    background: ghostwhite;
+    box-shadow: 0 px2rem(-8) px2rem(8) rgba(0, 0, 0, .15);
+
+    .icon-wrapper {
+      flex: 1;
+      @include center;
+
+      .icon-progress {
+        font-size: px2rem(28);
+      }
+
+      .icon-bright {
+        font-size: px2rem(24);
+      }
+
+    }
+
+    &.slide-up-enter, &slide-up-leave-to {
+      transform: translate3d(0, 100%, 0);
+    }
+
+    &.slide-up-enter-to, &slide-up-leave {
+      transform: translate3d(0, 0, 0);
+    }
+
+    &.slide-up-enter-active, &slide-up-leave-active {
+      transition: all .1s linear;
     }
   }
 }
