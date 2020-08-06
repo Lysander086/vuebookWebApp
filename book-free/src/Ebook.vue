fa0d000ca0d5d9f5db2e5192106aa71cc2412c3d
z <Lysander@example.com>
2020-08-06 15:38:24 +0800

    标题栏和菜单栏开发(1)  adjusting ebook style

--- book-free/src/Ebook.vue
+++ book-free/src/Ebook.vue
@@ -1,12 +1,20 @@
 <template>
   <div class="ebook">
-    <div class="read-wrapper">
+    <div class="title-wrapper">
+      <div class="left"><span class="icon-back icon"></span></div>
+      <div class="right icon-wrapper">
+        <div><span class="icon-cart icon"></span></div>
+        <div><span class="icon-person icon"></span></div>
+        <div><span class="icon-more icon"></span></div>
+      </div>
+    </div>
+    <div class="read-wrapper">1
       <div id="read">
         <div class="mask">
           <div class="left" @click="prevPage">
 
           </div>
-          <div class="center">
+          <div class="center" @click="toggleTitleAndMenu">
 
           </div>
           <div class="right" @click="nextPage">
@@ -35,10 +43,10 @@ export default {
       this.rendition.display()
 
     },
-    prevPage(){
+    prevPage() {
       this.rendition && this.rendition.prev()
     },
-    nextPage(){
+    nextPage() {
       this.rendition && this.rendition.next()
     }
   },
@@ -53,6 +61,37 @@ export default {
 
 .ebook {
   position: relative;
+
+  .title-wrapper {
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 101;
+    display: flex;
+    width: 100%;
+    height: px2rem(48);
+    background: ghostwhite;
+    box-shadow: 0 px2rem(8) px2rem(8) rgba(0, 0, 0, .15);
+
+    .left {
+      flex: 0 0 px2rem(60);
+      @include center;
+    }
+
+    .center {
+    }
+
+    .right {
+      flex: 1;
+      display: flex;
+      justify-content: flex-end;
+
+      .icon-wrapper {
+        flex: 0 0 px2rem(40);
+        @include center;
+      }
+    }
+  }
 
   .read-wrapper {
     .mask {
