0cf0c064b6eb006c63cf5b527a23329331f032a4
z <Lysander@example.com>
2020-08-07 22:38:40 +0800

    4-3 翻页操作实现

--- book-chapter/src/components/ebook/EbookReader.vue
+++ book-chapter/src/components/ebook/EbookReader.vue
@@ -7,20 +7,66 @@
 
 <script>
 import Epub from 'epubjs'
+import {mapGetters} from 'vuex'
 
 global.ePub = Epub
+
+
 export default {
   name: "EbookReader",
+  computed: {
+    ...mapGetters(['fileName'])
+  },
   methods: {
-    initEpub(){
+    prevPage() {
+      this.rendition && this.rendition.prev()
+    },
+    nextPage() {
+      this.rendition && this.rendition.next()
+    },
+    toggleTitleAndMenu() {
+
+    },
+
+    initEpub() {
+      const url = ' http://192.168.3.10:8081/epub/' + this.fileName + '.epub'
+      this.book = new Epub(url)
+      this.rendition = this.book.renderTo('read', {
+        width: innerWidth,
+        height: innerHeight,
+        method: 'default'
+      })
+      this.rendition.display()
+      // console.log(this.rendition)
+      this.rendition.on('touchstart', event => {
+        // console.log(event)
+        this.touchStartX = event.changedTouches[0].clientX
+        this.touchStartTime = event.timeStamp
+      })
+      // const offset
+      this.rendition.on('touchend', event => {
+        const offsetX = event.changedTouches[0].clientX - this.touchStartX
+        const time = event.timeStamp - this.touchStartTime
+        console.log(offsetX, time)
+        if (time < 500 && offsetX > 40) {
+          this.prevPage()
+        } else if (time < 500 && offsetX < -40) {
+          this.nextPage()
+        } else {
+          this.toggleTitleAndMenu()
+        }
+        event.preventDefault()
+        event.stopPropagation()
+
+      })
     }
   },
   mounted() {
-    const baseUrl = ' http://192.168.3.10:8081/epub/'
     const fileName = this.$route.params.fileName.split('|').join('/')
-    console.log(`${baseUrl}${fileName}.epub`)
-    // 以下为可尝试路径, 需要在目的目录中有一个try.epub
-    // http://localhost:8080/ebook/History%7Ctry
+    this.$store.dispatch('setFileName', fileName).then(() => {
+      this.initEpub()
+    })
+
   },
   data() {
     return {};
